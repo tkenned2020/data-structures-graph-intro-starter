2833e0c57fb6ed7c32647767c0617775da5aa269
tkenned2020 <tkenned2020@outlook.com>
2021-04-28 17:10:50 -0400

    breadthFirsttraversal in progress

--- problems/graph.js
+++ problems/graph.js
@@ -38,7 +38,26 @@ class Graph {
   }
 
   breadthFirstTraversal(startingVertex) {
-    
+    // const visited = new Set();
+    // if(!visited.has(startingVertex)){
+    //   visited.add(startingVertex)
+    // }
+    //if first neighbor is not in visited set,
+    //go to that node
+    //add b to visited/ then check the b's first
+    //N is in visited.has(a) if not keep traversing
+    const visited = new Set();
+    const q = [startingVertex]
+    while(q.length) {
+      let currentNode = q.shift()
+      if(!visited.has(currentNode)){
+          visited.add(currentNode)
+          q.push(this.adjList[currentNode][0])
+    }
+    console.log(visited)
+    allKeys = Object.keys(visited)
+
+
   }
 
   depthFirstTraversalIterative(startingVertex) {
@@ -54,6 +73,29 @@ class Graph {
   }
 }
 
+let graph;
+    const edges =
+        [['a', 'b'],
+        ['a', 'c'],
+        ['a', 'd'],
+        ['d', 'g'],
+        ['b', 'c'],
+        ['b', 'e'],
+        ['c', 'f'],
+        ['c', 'g'],
+        ['f', 'g'],
+        ['h']]
+
+  graph = new Graph();
+    for (let edge of edges) {
+        if (edge.length === 1) {
+            graph.addVertex(edge[0])
+        } else {
+            graph.addEdges(edge[0], edge[1])
+        }
+    }
+
+    console.log(graph)
 module.exports = {
   Graph,
 };
